903e5a746fff01423aa6630ac44c2a217039623c
Fritz Grobbelaar <fritz.grobbelaar@gmail.com>
2025-03-01 15:52:09 +0000

    Added a router

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { AccountListComponent } from './account-list/account-list.component';
 import { TransactionHistoryComponent } from './transaction-history/transaction-history.component';
+import { RouterOutlet, RouterLink } from '@angular/router';
+import { routes } from './app.routes'; // Import your routes
 
 @Component({
   selector: 'app-root',
-  imports: [AccountListComponent, TransactionHistoryComponent],
+  //imports: [AccountListComponent, TransactionHistoryComponent, Route],
+  imports: [RouterOutlet, RouterLink],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+//import { HomeComponent } from './home/home.component';
+//import { AboutComponent } from './about/about.component';
+import { AccountListComponent } from './account-list/account-list.component';
+import { TransactionHistoryComponent } from './transaction-history/transaction-history.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'accounts', component: AccountListComponent },
+  { path: 'transactions', component: TransactionHistoryComponent },
+  { path: '', redirectTo: '/accounts', pathMatch: 'full' },
+  { path: '**', redirectTo: '/accounts' },
+];
